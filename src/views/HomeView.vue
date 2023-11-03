e197928b2041557b3eb9902931058f12619eb306
Yurii Rybachok <drummer2009@ukr.net>
2023-11-03 18:32:53 +0200

    create home page with logout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,38 @@
 <template>
   <div class="home">
-   Home
+   <h1>Welcome, {{ name }}</h1>
+   <router-link to="/about">About</router-link>
+   <br>
+   <button class="logout" @click="Logout">Logout</button>
   </div>
 </template>
 
 <script>
-
+import { ref, onBeforeMount } from 'vue';
+import firebase from 'firebase/compat/app';
 export default {
   name: 'HomeView',
+  setup() {
+    const name = ref('');
+    
+    onBeforeMount(() => {
+      const user = firebase.auth().currentUser;
+      if (user) {
+        name.value = user.email.split('@')[0];
+      }
+    });
+
+    const Logout = () => {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => console.log("Signed out"))
+        .catch(err => alert(err.message));
+    }
+    return {
+      Logout,
+      name,
+    }
+  }
 }
 </script>
