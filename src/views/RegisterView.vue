4b9cf5c18ca40bbe609ac78321776ef7755a1a7c
Yurii Rybachok <drummer2009@ukr.net>
2023-11-03 17:52:36 +0200

    create register page

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,15 +1,39 @@
 <template>
-    <div class="register">
-        Register
-    </div>
+  <div class="register">
+    <h1>Register</h1>
+    <form @submit.prevent="Register">
+      <input type="email" placeholder="Email" v-model="email" />
+      <input type="password" placeholder="Password" v-model="password" />
+      <input type="submit" value="Register" />
+      <p>Have an account? <router-link to="/login">Login Here</router-link></p>
+    </form>
+  </div>
 </template>
 
 <script>
-    export default {
-        
+import { ref } from 'vue';
+import firebase from 'firebase/compat/app';
+export default {
+  setup() {
+    const email = ref('');
+    const password = ref('');
+
+    const Register = () => {
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(email.value, password.value)
+            .then(user => {
+                alert(user);
+            })
+            .catch(err => alert(err.message));
     }
+    return {
+        Register,
+        email,
+        password,
+    }
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
