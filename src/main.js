7471ba66e6bbd15c68b793ccf745947d69425638
Yurii Rybachok <drummer2009@ukr.net>
2023-11-03 16:04:29 +0200

    setup project & add firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import firebase from 'firebase/compat/app';
 
-createApp(App).use(router).mount('#app')
+const firebaseConfig = {
+  apiKey: 'AIzaSyBJ0PD-Vc4zHQ0CtTPFes7fEGndsUvU0mE',
+  authDomain: 'vue-authentication-fe1ac.firebaseapp.com',
+  projectId: 'vue-authentication-fe1ac',
+  storageBucket: 'vue-authentication-fe1ac.appspot.com',
+  messagingSenderId: '887786287022',
+  appId: '1:887786287022:web:249f6a37b9513b28cd623f',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+createApp(App).use(router).mount('#app');
